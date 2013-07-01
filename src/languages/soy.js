d0b6e4ae817d54da164ec2f63cd3be018387b6ec
Ben Wong <bewong@atlassian.com>
2013-07-01 10:56:49 +1000

    Adding Google Closure Templates (Soy) to supported languages

---
+++ src/languages/soy.js
@@ -0,0 +1,81 @@
+/*
+Language: Soy Templates
+Author: Ben Wong <bewong@atlassian.com>
+Description: A Highlighter for soy (google closure) templates
+*/
+function(hljs) {
+
+
+    var soyKeywords = {
+        keyword: 'or and not if delpackage alias noAutoescape call literal private data print msg let if elseif else switch case default foreach ifempty for call param css',
+        tag: 'template call param namespace sp nil \\r \\n \\t lb rb'
+    };
+
+    var param = {
+        className: "symbol",
+        begin: "\\$[a-zA-Z][a-zA-Z0-9_]*"
+    };
+
+    var soyString = {
+        className: "string",
+        begin: "\"",
+        end: "[\"\\n]",
+        contains: [
+            param
+        ]
+    };
+
+    var soyStringSingle = {
+        className: "string",
+        begin: "'",
+        end: "['\\n]"
+    };
+
+    var soyTags = {
+        className: "soyTag",
+        begin: "{",
+        end: "}",
+        keywords: soyKeywords,
+        contains: [
+            soyString,
+            soyStringSingle,
+            param,
+            {
+                className: "title",
+                begin: "(namespace |template |call )[a-zA-Z0-9_.]*",
+                endsWithParent: true,
+                keywords: soyKeywords
+            },
+            {
+                className: "symbol",
+                begin: "param [a-zA-Z0-9_.]*",
+                end: ":",
+                endsWithParent: true,
+                returnEnd: true,
+                keywords: soyKeywords
+            }
+        ]
+    };
+    return {
+        keywords: "or and not",
+        contains: [
+            param,
+            soyString,
+            soyStringSingle,
+            soyTags,
+            {
+                className: "comment",
+                begin: "/\\*",
+                end: "\\*/",
+                contains: [
+                    {
+                        className: "attribute",
+                        begin: "@param\\??\\W",
+                        end: "\\W",
+                        excludeBegin: true
+                    }
+                ]
+            }
+        ]
+    };
+}
